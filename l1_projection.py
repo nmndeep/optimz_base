17287cea708adfdb3f36225e2b0c5fc94273b2ab
nmndeep <nmndeep@gmail.com>
2020-04-01 23:55:22 +0200

    l1_proj added

--- l1_projection.py
+++ l1_projection.py
@@ -1,73 +1,38 @@
+
+'''Using the algorithm by Duchi 2008, projection on l1-ball using a positive simplex, optimum lies on the boundary if not already within the ball,
+Complexity O(nlog(n)) '''
 import numpy as np
 
-def euclidean_proj_simplex(v, s=1):
-    """ Compute the Euclidean projection on a positive simplex
-    Solves the optimisation problem (using the algorithm from [1]):
-        min_w 0.5 * || w - v ||_2^2 , s.t. \sum_i w_i = s, w_i >= 0
-    Parameters
-    ----------
-    v: (n,) numpy array,
-       n-dimensional vector to project
-    s: int, optional, default: 1,
-       radius of the simplex
-    Returns
-    -------
-    w: (n,) numpy array,
-       Euclidean projection of v on the simplex
-    Notes
-    -----
-    O(n log(n))
-    """
-    assert s > 0, "Radius s must be strictly positive (%d <= 0)" % s
-    n, = v.shape  # will raise ValueError if v is not 1-D
-    # check if we are already on the simplex
-    if v.sum() == s and np.alltrue(v >= 0):
-        # best projection: itself!
+def proj_pos_simplex(v, r=1):
+
+    assert r > 0, "Radius must be positive (%d <= 0)" % s
+    n, = v.shape
+    #  if  already on the simplex
+    if v.sum() == r and np.alltrue(v >= 0):
         return v
     # get the array of cumulative sums of a sorted (decreasing) copy of v
-    u = np.sort(v)[::-1]
-    cssv = np.cumsum(u)
+    mu = np.sort(v)[::-1]
+    cssv = np.cumsum(mu)
     # get the number of > 0 components of the optimal solution
-    rho = np.nonzero(u * np.arange(1, n+1) > (cssv - s))[0][-1]
+    rho = np.nonzero(mu * np.range(1, n+1) > (cssv - r))[0][-1]
     # compute the Lagrange multiplier associated to the simplex constraint
-    theta = float(cssv[rho] - s) / rho
-    # compute the projection by thresholding v using theta
+    theta = float(cssv[rho] - r) / rho
+    # set w = max(v-theta, 0)
     w = (v - theta).clip(min=0)
     return w
 
 
-def euclidean_proj_l1ball(v, s=1):
-    """ Compute the Euclidean projection on a L1-ball
-    Solves the optimisation problem (using the algorithm from [1]):
-        min_w 0.5 * || w - v ||_2^2 , s.t. || w ||_1 <= s
-    Parameters
-    ----------
-    v: (n,) numpy array,
-       n-dimensional vector to project
-    s: int, optional, default: 1,
-       radius of the L1-ball
-    Returns
-    -------
-    w: (n,) numpy array,
-       Euclidean projection of v on the L1-ball of radius s
-    Notes
-    -----
-    Solves the problem by a reduction to the positive simplex case
-    See also
-    --------
-    euclidean_proj_simplex
-    """
-    assert s > 0, "Radius s must be strictly positive (%d <= 0)" % s
-    n, = v.shape  # will raise ValueError if v is not 1-D
-    # compute the vector of absolute values
-    u = np.abs(v)
-    # check if v is already a solution
-    if u.sum() <= s:
-        # L1-norm is <= s
-        return v
-    # v is not already a solution: optimum lies on the boundary (norm == s)
-    # project *u* on the simplex
-    w = euclidean_proj_simplex(u, s=s)
-    # compute the solution to the original problem on v
-    w *= np.sign(v)
-    return w
+
+""" Compute the Euclidean projection on a L1-ball"""
+r = 1 # radius of the ball, should be +ve
+v = np.random.rand(25) ###   vector to be projected
+n, = v.shape
+mu = np.abs(v)
+# check if v is already a solution
+if mu.sum() <= r:
+    w = v
+    break
+w = proj_pos_simplex(mu, r=r)
+# Just add the sign of the corresponding element of v to each element of w
+w*= np.sign(v)
+print(w)

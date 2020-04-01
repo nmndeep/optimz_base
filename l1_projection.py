8cde848499479f748aa75b44e30f06d5229081d8
nmndeep <nmndeep@gmail.com>
2020-04-01 23:57:01 +0200

    l1_proj added

--- l1_projection.py
+++ l1_projection.py
@@ -1,6 +1,7 @@
-
+'''Naman Deep Singh 01.04.2020'''
 '''Using the algorithm by Duchi 2008, projection on l1-ball using a positive simplex, optimum lies on the boundary if not already within the ball,
 Complexity O(nlog(n)) '''
+
 import numpy as np
 
 def proj_pos_simplex(v, r=1):
@@ -10,18 +11,12 @@ def proj_pos_simplex(v, r=1):
     #  if  already on the simplex
     if v.sum() == r and np.alltrue(v >= 0):
         return v
-    # get the array of cumulative sums of a sorted (decreasing) copy of v
+
     mu = np.sort(v)[::-1]
-    cssv = np.cumsum(mu)
-    # get the number of > 0 components of the optimal solution
-    rho = np.nonzero(mu * np.range(1, n+1) > (cssv - r))[0][-1]
-    # compute the Lagrange multiplier associated to the simplex constraint
+    rho = np.nonzero(mu * np.range(1, n+1) > (np.cumsum(mu) - r))[0][-1]
     theta = float(cssv[rho] - r) / rho
-    # set w = max(v-theta, 0)
     w = (v - theta).clip(min=0)
     return w
-
-
 
 """ Compute the Euclidean projection on a L1-ball"""
 r = 1 # radius of the ball, should be +ve
